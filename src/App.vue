59701190e6d3d2f81ff1b89388b8fa5a28fc0876
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-06 16:25:20 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div id="app">
-  
+  <Header/>
   </div>
 </template>
 
 <script>
-
+import Header from "@/components/Header.vue";
 export default {
   name: 'App',
   components: {
-  
+   Header,
   }
 }
 </script>
 
 <style lang="scss">
+*{
+  padding: 0;
+  margin:0;
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
 
+body{
+  background-color: #000;
+}
+
+img{
+  width:100%;
+  height: auto;
+}
+
+.container{
+  max-width: 1500px;
+  margin: 0 auto;
+}
+
+.row{
+  display: flex;
+
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,119 @@
+<template>
+  <header>
+    <div class="row container">
+      <!-- logo + nav links -->
+      <div class="col">
+        <img src="@/assets/img/dc-logo.png" alt="logo dc" />
+      </div>
+      <nav class="col">
+        <ul id="list-nav">
+          <li v-for="(link, index) in links" :key="index">
+            <a :href="link.url" :class="{active: link.current}">{{link.text}}</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+
+export default {
+ name: 'Header',
+ data(){
+   return {
+       links:[
+        {
+            text: 'Characters',
+            url: '#',
+            current: false,
+        },
+        {
+            text: 'Comics',
+            url: '#',
+            current: true,
+        },
+        {
+            text: 'Movies',
+            url: '#',
+            current: false,
+        },
+        {
+            text: 'TV',
+            url: '#',
+            current: false,
+        },
+        {
+            text: 'Games',
+            url: '#',
+            current: false,
+        },
+        {
+            text: 'Collectibles',
+            url: '#',
+            current: false,
+        },
+        {
+            text: 'Videos',
+            url: '#',
+            current: false,
+        },
+        {
+            text: 'Fans',
+            url: '#',
+            current: false,
+        },
+        {
+            text: 'News',
+            url: '#',
+            current: false,
+        },
+        {
+            text: 'Shop',
+            url: '#',
+            current: false,
+        },
+     ],
+   }
+  },
+}
+</script>
+
+<style>
+header {
+  background-color: #fff;
+  height: 12vh;
+}
+
+header .row {
+  height: 100%;
+  justify-content: space-between;
+  align-items: center;
+}
+#list-nav {
+  display: flex;
+  justify-content: space-between;
+}
+
+#list-nav li {
+  list-style-type: none;
+}
+
+#list-nav li a {
+  text-decoration: none;
+  padding: 44px 20px;
+  color: #000;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 0.9em;
+}
+
+#list-nav li a:hover {
+  color: #0282f9;
+}
+
+.active{
+  border-bottom: 4px solid #0282f9;
+  color: #0282f9;
+}
+</style>
